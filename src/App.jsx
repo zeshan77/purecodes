be0bd4105eb9316c78a5b5c4159d559bcc076c89
imuhammadessa <imuhammadessa@gmail.com>
2025-09-30 19:15:31 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl text-center text-teal-700 h-screen bg-gray-100 p-4'>
+      <h1>Hello Purecodes</h1>
+    </div>
+  )
+}
+
+export default App
