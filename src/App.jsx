adcab640881d2560851f13541f10efbad6a0f6b6
imuhammadessa <imuhammadessa@gmail.com>
2025-10-06 17:15:07 +0500

    basic config fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./layout/Layout";
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes/AppRoutes'
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-        </Routes>
-      </Layout>
-    </Router>
-  );
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
 }
-
-export default App;
